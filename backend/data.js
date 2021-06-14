c540ee935cf138f129cba1e2efb778d768535660
Blooshie <josipmiki53@gmail.com>
2021-06-14 17:19:02 +0200

    working frontend with mockup data

---
+++ backend/data.js
@@ -0,0 +1,58 @@
+export const data = {
+  worker: [
+    {
+      id: 100000,
+      name: "Marko",
+      surname: "Matic",
+      type: 1,
+    },
+    {
+      id: 100001,
+      name: "Miso",
+      surname: "Ivic",
+      type: 2,
+    },
+    {
+      id: 100002,
+      name: "Jure",
+      surname: "Katic",
+      type: 3,
+    },
+    {
+      id: 100003,
+      name: "Ivana",
+      surname: "Culina",
+      type: 1,
+    },
+  ],
+  entries: [
+    {
+      entry_id: 12,
+      user_id: 100003,
+      time_entry: "20-03-21/08:00:21",
+      date: "31-12-21",
+      location_id: 21,
+    },
+    {
+      entry_id: 212,
+      user_id: 100001,
+      time_entry: "20-03-21/08:00:21",
+      date: "31-12-21",
+      location_id: 21,
+    },
+    {
+      entry_id: 3012,
+      user_id: 100002,
+      time_entry: "20-03-21/08:00:21",
+      date: "31-12-21",
+      location_id: 23,
+    },
+    {
+      entry_id: 10012,
+      user_id: 100001,
+      time_entry: "08:00:21",
+      date: "31-12-21",
+      location_id: 22,
+    },
+  ],
+};
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+import path from "path";
+import express from "express";
+import { data } from "./data.js";
+var app = express();
+var port = 3001;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/data", (req, res) => {
+  res.send(data);
 });
 
 app.listen(port, () => {
